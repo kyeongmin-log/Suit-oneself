532e6776d5ec0f3e79111796f42e6feb184888a0
KyeongMin <dotcom7860@gmail.com>
2021-08-15 19:13:18 +0900

    JS : fetch practice

---
+++ kakao-book-search(api practice)/main.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const inputBookName = document.querySelector("#bookName");
+const searchBtn = document.querySelector("#search");
+
+// 책 검색
+function searchBooks() {
+  getBookData(inputBookName.value).then((books) => {
+    displayBooks(books);
+  });
+  inputBookName.value = "";
+  inputBookName.focus();
+}
+
+// 검색한 책의 데이터를 Kakao api를 통해 가져온다.
+function getBookData(bookName) {
+  return fetch(`https://dapi.kakao.com/v3/search/book?target=title&query=${bookName}`, {
+    headers: {
+      Authorization: "{카카오 API키 입력}",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => json.documents);
+}
+
+// 넘어온 데이터를 가지고 화면에 표시할 내용을 만든다.
+function createBook(book) {
+  return `
+  <li class="book">
+    <span class="title">${book.title}</span>
+      <img
+        src=${book.thumbnail}
+      />
+      <p>
+        ${book.contents === "" ? "(내용이 없습니다.)" : book.contents}
+      </p>
+  </li>
+  <hr />
+  `;
+}
+
+// 만든 내용을 화면에 표시한다.
+function displayBooks(books) {
+  const bookList = document.querySelector(".book-list");
+  bookList.innerHTML = books.map((book) => createBook(book)).join("");
+}
+
+// 클릭 검색
+searchBtn.addEventListener("click", searchBooks);
+//Enter 검색
+inputBookName.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    searchBooks();
+  }
+});
---
+++ kakao-book-search(api practice)/readme.md
@@ -0,0 +1,17 @@
+# kakao-book-search(api practice)
+
+> 카카오톡 api를 통해 api 연습해보기
+
+참고 강의 : https://www.youtube.com/watch?v=QPEUU89AOg8&t=115s
+
+# Result
+
+![result](./api-result.gif)
+
+# 리뷰
+
+강의에서는 JQuery를 사용하여 만들었지만 fetch를 이용해도 가능할 거 같아서 fetch로 만들게 되었다.
+
+api를 사용하니 재밌는 것들을 많이 만들 수 있을 거 같다.
+
+다음엔 미세먼지나 코로나 api를 이용해서 연습해봐야겠다.
